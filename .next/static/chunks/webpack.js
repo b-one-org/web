c6feb9b17032e8c3caf3ba98fd5e10632ee264ab
Chris <chrisferreyra13@gmail.com>
2021-02-11 18:13:54 -0300

    burguer menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4549e51931fb9717a79a";
+/******/ 	var hotCurrentHash = "d28e25469668522f22b5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
