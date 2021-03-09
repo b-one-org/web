6481309da3740cd9f68433fd1a87473227ca2232
Chris <chrisferreyra13@gmail.com>
2021-03-08 23:25:48 -0300

    arreglando el error de los imports

--- pages/challenges.js
+++ pages/challenges.js
@@ -2,15 +2,6 @@
 import loadable from '@loadable/component';
 import Container from '../components/Container';
 
-import JoinHeaderContent from '../content/join/JoinHeaderContent'
-import ExpAProjects from '../content/join/JoinExpAProjects.json'
-import ExpAProblems from '../content/join/JoinExpAProblems.json'
-import Community from '../content/join/JoinExpACommunity.json'
-import Principles from '../content/join/JoinPrinciples.json';
-import BenefitsEntrepreneurExpA from '../content/join/JoinBenefitsEntrepreneur.json';
-import BenefitsExpA from '../content/join/JoinBenefitsExpA.json';
-import ProvocativeEnding from '../content/join/JoinProvocativeMiddleBlock.json'
-
 
 const Header = loadable(() => import('../components/Header'));
 const MiddleBlock = loadable(() => import('../components/MiddleBlock'));
