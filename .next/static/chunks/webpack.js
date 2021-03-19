ce35fda2a7808bdbe67e69afcdcef78bdc45e192
Chris <chrisferreyra13@gmail.com>
2021-03-18 22:59:45 -0300

    logo nuevo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8567d445c81f10d176ff";
+/******/ 	var hotCurrentHash = "f1b2cbb39e054b31207a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
